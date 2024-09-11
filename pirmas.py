cd4f2ee9dbd8e67cff2e3518d970a762e43bd343
NIKASURG <Nsurgautas1.com>
2024-09-11 09:25:06 +0300

    pirma užduotis

---
+++ pirmas.py
@@ -0,0 +1,5 @@
+z = int(input('sraigės nušliauštaas atstumas per valanda matuojant žiogo pašokimu: '))
+c = int(input('vienas žiogo šuolis centimetrais: '))
+v = int(input('kiek laiko tai vyko: '))
+sAtstumas = z*c*v
+print('Sraigė nušliaušė'+ str(sAtstumas) + 'cm.')
