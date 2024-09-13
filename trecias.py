f87a2ea8bd6a1603d838e0fd6a419fae6ada732a
NIKASURG <Nsurgautas1.com>
2024-09-13 09:57:26 +0300

    le push

--- trecias.py
+++ trecias.py
@@ -2,5 +2,5 @@ n = int(input('Degtuku skaičius: '))
 m = int(input('Degtuko degimo laikas minutėmis:'))
 vis = n * m / 60
 val = int(n * m/60)
-min = (vis - val) * 60
-print('Degtukas degė: h '+str(val) + ' min'+str(min))
+min = int((vis - val) * 60)
+print('Degtukas degė: h '+str(val) + ' min '+str(min))
