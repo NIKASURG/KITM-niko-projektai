68d21932bf4dad4b04dc05ec68aacb987c122ef1
NIKASURG <Nsurgautas1.com>
2024-09-13 13:25:03 +0300

    sestas

---
+++ sestas.py
@@ -0,0 +1,6 @@
+x = int(input('Lektuvo ilgis: '))
+y = int(input('Kiek kartu lektuvas trumpesnis už pakilimo taka: '))
+plotis = x * 3
+ilgis =  plotis * y 
+plotas = plotis * ilgis
+print('Tako plotas:' + str(plotas)+ 'm' + 'ilgis: ' + str(ilgis) + 'm')
