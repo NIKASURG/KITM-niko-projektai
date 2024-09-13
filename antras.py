76183443a8515830bccb09ed217c368a6f478b52
NIKASURG <Nsurgautas1.com>
2024-09-13 07:18:40 +0300

    antras darbas

---
+++ antras.py
@@ -0,0 +1,5 @@
+k = int(input('laivų skaičius: '))
+n = int(input('maišų kiekis laive: '))
+m = int(input('nuskendusių laivų kiekis: '))
+ats = (k-m) * n
+print(str(ats) + ' maišų nukeliavo saugiai.')
