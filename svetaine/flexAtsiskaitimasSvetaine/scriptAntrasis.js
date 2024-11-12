81cdbbddc6d89fed8440260ce732ed0466ecc93f
nikas <nsurgautas1@gmail.com>
2024-11-12 16:28:51 +0200

    saving

--- svetaine/flexAtsiskaitimasSvetaine/scriptAntrasis.js
+++ svetaine/flexAtsiskaitimasSvetaine/scriptAntrasis.js
@@ -2,16 +2,34 @@ window.addEventListener("load", () => {
   let uzrasas = document.getElementById("uzrasas");
   let langelis = document.getElementById("lamgelis");
   let Ikelti = document.getElementById("Ikelti");
+  console.log(localStorage.getItem("1"));
+ 
+  const pirmas = document.getElementById("primas");
+  const antras = document.getElementById("antras");
+  const trecias = document.getElementById("trecias");
+  const ketvirtas = document.getElementById("ketvirtas");
+  const penktas = document.getElementById("penktas");
   let pirm = "";
   let ant = "";
   let tre = "";
   let ket = "";
   let pen = "";
-  let pirmas = document.getElementById("pirmas");
-  let antras = document.getElementById("antras");
-  let trecias = document.getElementById("trecias");
-  let ketvirtas = document.getElementById("ketvirtas");
-  let penktas = document.getElementById("penktas");
+
+  if(localStorage.getItem("1") == null){
+    
+  }
+ else{
+  pirm =localStorage.getItem("1") 
+  ant =localStorage.getItem("2")
+  tre =localStorage.getItem("3")
+  ket =localStorage.getItem("4")
+  pen =localStorage.getItem("5")
+  pirmas.innerHTML = pirm;
+  antras.innerHTML = ant;
+  trecias.innerHTML = tre;
+  ketvirtas.innerHTML = ket;
+  penktas.innerHTML = pen;
+ }
   Ikelti.addEventListener("click", function () {
     uzrasas = document.getElementById("uzrasas");
     langelis = document.getElementById("lamgelis");
@@ -53,8 +71,15 @@ window.addEventListener("load", () => {
     antras.innerHTML = ant;
     trecias.innerHTML = tre;
     ketvirtas.innerHTML = ket;
-    penktas.innerHTML = penktas;
+    penktas.innerHTML = pen;
     console.log(uzrasas.value);
     console.log("test");
+    localStorage.setItem("1", pirm);
+    localStorage.setItem("2", ant);
+
+    localStorage.setItem("3", tre);
+
+    localStorage.setItem("4", ket);
+    localStorage.setItem("5", pen);
   });
 });
